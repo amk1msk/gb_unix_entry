c69d20c8f7e4c3ca661ced6aa74674e3a2c1bf14
amk1msk <52780070+amk1msk@users.noreply.github.com>
2021-02-01 22:00:01 +0300

    Update and rename homework_2 to homework_2.sh

--- homework_2.sh
+++ homework_2.sh
@@ -1,32 +1,32 @@
-1
+#1
 ls /etc
 ls /proc
 ls /home
 cat /etc/adduser.conf
 cat /etc/bash.bashrc
 
-2
+#2
 cat /etc/adduser.conf > ~/1
 cat /etc/bash.bashrc > ~/2
 cat ~/1 ~/2 > ~/3
 less ~/3
 mv ~/3 ~/4
 
-3
+#3
 touch 5
 touch 6
 mkdir 7
 mv 5 7
 rm -r *
 
-4
+#4
 ls -lA ~/ | awk '{print$9}' | grep "^\." | wc -l
 
-5
+#5
 cat /etc/* 2> ~/stderr_etc
 less stderr_etc | grep denied | wc -l
 
-6
+#6
 gedit
 ps aux | grep gedit
 kill -15 28016
@@ -40,17 +40,17 @@ Killed
 kill -1 28140
 Hangup
 
-7
+#7
 lsof -u user | grep "/dev" | less
 
-8
+#8
 mkdir -p 20{15..20}/{01..12}
 
-9
+#9
 cd ~/
 touch ~/20{15..20}{01..12}{01..30}{01..03}.txt
 for i in *.txt; do echo $i > $i; done
 for i in {2015..2020}; do for j in {01..12}; do mv ~/$i$j*.txt ~/$i/$j; done; done
 
-10
+#10
 ls -lA ~/ | cut -b 1-10 | grep -v total | sort -u | wc -l
